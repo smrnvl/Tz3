7ed30823405159bd76b0ce4725c7e334fe4311bc
smrnvl <smirnov.waxmatist@mail.ru>
2021-12-19 01:33:41 +0300

    Исправлен тестовый набор данных с учётом специфики вещественных чисел

--- tests.py
+++ tests.py
@@ -22,14 +22,14 @@ class Tests(unittest.TestCase):
 
     def test_summa(self):
         self.assertEqual(sum_of_nums([5.0, -0.0, -5.0]), 0)
-        self.assertEqual(sum_of_nums([-5.3, 1.2, 1.1]), -3)
+        self.assertEqual(sum_of_nums([-5.0, 1.2, 1.0]), -2.8)
         self.assertEqual(sum_of_nums([-0.1, -0.01, -0.001]), -0.111)
-        self.assertEqual(sum_of_nums([1.0, 8.3, 8.0]), 17.3)
+        self.assertEqual(sum_of_nums([1.0, 8.2, 8.0]), 17.2)
         self.assertEqual(sum_of_nums([-9.0, -123.6, -9.1]), -141.7)
 
     def test_multiplication(self):
         self.assertEqual(multiplication_of_nums([5.0, -0.0, -5.0]), 0)
-        self.assertEqual(multiplication_of_nums([-5.3, 1.2, -2.0]), 12.72)
+        self.assertEqual(multiplication_of_nums([-5.0, 1.2, -2.0]), 12)
         self.assertEqual(multiplication_of_nums([-0.1, -0.01, -0.001]), -0.000001)
         self.assertEqual(multiplication_of_nums([4.0, 4.0, 4.0]), 64)
         self.assertEqual(multiplication_of_nums([-3.0, -3.0, -3.0]), -27)
